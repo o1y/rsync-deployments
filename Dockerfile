5413f94fd414b7adf82ac4f61af1be6befd7d9b2
Oliver Mesieh <olivermesieh@gmail.com>
2021-03-18 19:34:01 +0100

    Add rsync without remote ssh key

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM drinternet/rsync:v1.4.1
 
+# Install sshpass
+RUN apk update && apk add --no-cache sshpass
+RUN rm -rf /var/cache/apk/*
+
 # Copy entrypoint
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# Start the SSH agent and load key.
+# Start the SSH agent
 source agent-start "$GITHUB_ACTION"
-echo "$INPUT_REMOTE_KEY" | SSH_PASS="$INPUT_REMOTE_KEY_PASS" agent-add
 
 # Add strict errors.
 set -eu
@@ -12,6 +11,7 @@ SWITCHES="$INPUT_SWITCHES"
 RSH="ssh -o StrictHostKeyChecking=no -p $INPUT_REMOTE_PORT $INPUT_RSH"
 LOCAL_PATH="$GITHUB_WORKSPACE/$INPUT_PATH"
 DSN="$INPUT_REMOTE_USER@$INPUT_REMOTE_HOST"
+SSHPASS="sshpass -p $INPUT_REMOTE_PASSWORD"
 
 # Deploy.
-sh -c "rsync $SWITCHES -e '$RSH' $LOCAL_PATH $DSN:$INPUT_REMOTE_PATH"
+sh -c "sshpass -p $INPUT_REMOTE_PASSWORD rsync $SWITCHES -e '$RSH' $LOCAL_PATH $DSN:$INPUT_REMOTE_PATH"
